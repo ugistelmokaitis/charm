74c0f9c4955907d76b7e27d0ae8354031f05a41e
aezthetics <hello@ugistelmokaitis.com>
2022-04-27 14:41:01 +1000

    Add[externalLink component]: added externalLink component

---
+++ components/externalLink.tsx
@@ -0,0 +1,13 @@
+import type { LinkProps } from '@prismicio/react';
+import Link from 'next/link';
+import type { FC } from 'react';
+
+const ExternalLinkComponent: FC<LinkProps> = ({ children, href, ...props }) => (
+  <Link href={href}>
+    <a {...props} target="_blank" rel="noopener noreferrer" className="">
+      {children}
+    </a>
+  </Link>
+);
+
+export default ExternalLinkComponent;
