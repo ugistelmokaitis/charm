e100cb8a261d481dcf782c4f068b3cb699879aa8
aezthetics <hello@ugistelmokaitis.com>
2022-04-27 14:29:53 +1000

    Add[next config]: next config pwa, prismic, webpeck configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const {createSecureHeaders} = require('next-secure-headers');
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+const {withSentryConfig} = require('@sentry/nextjs');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          disable: process.env.NODE_ENV === "development",
+          dest: 'public',
+          dynamicStartUrl: false,
+          mode: process.env.NODE_ENV,
+        },
+      },
+      [
+        withSentryConfig,
+        {
+          silent: true,
+        },
+      ],
+      [withBundleAnalyzer],
+    ]
+  ],
+  {
+
+
+    async headers() {
+      return [{source: "/(.*)", headers: createSecureHeaders()}];
+    },
+
+
+    images: {
+      domains: ['images.prismic.io', 'charm.cdn.prismic.io'],
+      disableStaticImages: true,
+    },
+
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+      return config;
+
+    }
+  });
