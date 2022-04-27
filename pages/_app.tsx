cecf5c85fa1c7b0b346a150f82ebf48da48d01a3
aezthetics <hello@ugistelmokaitis.com>
2022-04-27 14:36:49 +1000

    Update[_app.tsx]: added prismic provider, toaster and the rest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { PrismicProvider } from '@prismicio/react';
+import type { FC } from 'react';
+import { Toaster } from 'react-hot-toast';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { client, linkResolver } from '../utils/prismic';
+import ExternalLinkComponent from '../components/externalLink';
+import InternalLinkComponent from '../components/internalLink';
+import richTextComponents from '../components/richTextComponents';
 
-export default MyApp
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={InternalLinkComponent}
+      externalLinkComponent={ExternalLinkComponent}
+      client={client}
+      richTextComponents={richTextComponents}
+    >
+      <Component {...pageProps} />
+    </PrismicProvider>
+    <Toaster
+      toastOptions={{
+        position: 'bottom-right',
+      }}
+    />
+  </>
+);
+
+export default MyApp;
