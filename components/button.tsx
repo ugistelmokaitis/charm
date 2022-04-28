2549ac2c158ef0f410ba3dfb41a1158e3e5ce51f
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-04-29 01:13:25 +1000

    Update[button component]: fixed button css

--- components/button.tsx
+++ components/button.tsx
@@ -14,9 +14,9 @@ type ButtonProps = LinkProps & {
 const classes = {
   variant: {
     primary:
-      'bg-primary-100 text-neutral-0 outline-[0.0625rem] active:outline-[0.125rem] disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none active:outline-primary-50',
+      'bg-primary-100 text-neutral-0 outline-[0.0625rem] active:outline-[0.125rem] disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none active:outline-primary-50 outline-none',
     secondary:
-      'outline-[0.0625rem] hover:bg-primary-25 outline-neutral-65 active:outline-[0.125rem] active:outline-primary-200 disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none dark:bg-neutral-100 dark:text-neutral-0 dark:disabled:bg-neutral-80 dark:disabled:text-neutral-50 dark:disabled:hover:outline-none dark:active:outline-neutral-15',
+      'outline outline-[0.0625rem] hover:bg-primary-25 outline-neutral-65 active:outline-[0.125rem] active:outline-primary-200 disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none dark:bg-neutral-100 dark:hover:bg-neutral-65 dark:text-neutral-0 dark:disabled:bg-neutral-80 dark:disabled:text-neutral-50 dark:disabled:hover:outline-none dark:active:outline-neutral-15 dark:active:bg-neutral-65',
   },
 };
 
@@ -28,7 +28,7 @@ const Button: FC<ButtonProps> = ({
   ...props
 }) => (
   <a
-    className={`flex items-center justify-center rounded py-[0.7188rem] px-[1.8175rem] outline
+    className={`flex items-center justify-center rounded py-[0.7188rem] px-[1.8175rem]
     ${classes.variant[variant]} ${className}`}
     {...props}
   >
