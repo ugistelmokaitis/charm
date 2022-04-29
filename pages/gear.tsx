957031a2806696b3829afde9068ed0f562f31759
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-04-29 22:08:52 +1000

    Update[gear page]: add dark mode css

--- pages/gear.tsx
+++ pages/gear.tsx
@@ -16,24 +16,26 @@ type IGear = {
 
 const Gear: FC<IGear> = ({ data, settings }) => (
   <Layout title="" description="" settings={settings}>
-    <div className="bg-black-100  selection:bg-purple-100 selection:text-white-100 lg:grid-cols-12">
+    <div className="flex bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100">
       <Container>
-        <div className="grid-starts-1 grid grid-cols-12 gap-16 pt-32 text-white-100 md:grid-cols-12 md:gap-8">
-          <div className=" col-span-4 ">
+        <div className="grid-starts-1 text-white-100 grid grid-cols-12 gap-16 pt-32 md:grid-cols-12 md:gap-8">
+          <div className=" col-span-6">
             <PrismicRichText field={data.introTitle} />
             <div className="mb-28 mt-28">
-              <PrismicRichText field={data.introDescription} />
+              <div className="font-ABCWhyteEdu_Regular text-pLGRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+                {data.introDescription}
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-16 text-white-100 md:grid-cols-12 md:gap-8">
+        <div className="text-white-100 grid grid-cols-1 gap-16 md:grid-cols-12 md:gap-8">
           <div className="col-span-3">
-            <div className="pb-6 font-codeSemiBold text-codeMDSemiBold font-semibold text-purple-100">
-              <PrismicRichText field={data.deviceTitle} />
+            <div className="font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-neutral-100 dark:text-blue-100">
+              {data.deviceTitle}
             </div>
-            <div className="pb-20 font-charmRegular text-pLGRegular font-normal tracking-[0.02em] text-lightestGrey-100">
-              {asText(data.deviceDescription)}
+            <div className="pt-6 pb-20 font-ABCWhyteEdu_Regular text-pLGRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+              {data.deviceDescription}
             </div>
           </div>
         </div>
@@ -50,21 +52,21 @@ const Gear: FC<IGear> = ({ data, settings }) => (
                   layout="fixed"
                   quality={100}
                 />
-                <div className="max-w-[16.5rem] pt-8 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100 ">
-                  <PrismicRichText field={deviceDetails} />
+                <div className="pt-8 font-ABCWhyteEdu_Regular text-pMDRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+                  {deviceDetails}
                 </div>
               </div>
             ))}
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-16 text-white-100 md:grid-cols-12 md:gap-8">
-          <div className="col-span-3">
-            <div className="pb-6 font-codeSemiBold text-codeMDSemiBold font-semibold text-purple-100">
-              <PrismicRichText field={data.setupTitle} />
+        <div className="text-white-100 grid grid-cols-1 gap-16 md:grid-cols-12 md:gap-8">
+          <div className="col-span-4">
+            <div className="font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-neutral-100 dark:text-blue-100">
+              {data.setupTitle}
             </div>
-            <div className="pb-20 font-charmRegular text-pLGRegular font-normal tracking-[0.02em] text-lightestGrey-100">
-              {asText(data.setupDescription)}
+            <div className="pt-6 pb-20 font-ABCWhyteEdu_Regular text-pLGRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+              {data.setupDescription}
             </div>
           </div>
         </div>
@@ -81,21 +83,21 @@ const Gear: FC<IGear> = ({ data, settings }) => (
                   layout="fixed"
                   quality={100}
                 />
-                <div className="max-w-[16.5rem] pt-8 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100 ">
-                  <PrismicRichText field={setupDetails} />
+                <div className="pt-8 font-ABCWhyteEdu_Regular text-pMDRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+                  {setupDetails}
                 </div>
               </div>
             ))}
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-16 text-white-100 md:grid-cols-12 md:gap-8">
-          <div className="col-span-3">
-            <div className="pb-6 font-codeSemiBold text-codeMDSemiBold font-semibold text-purple-100">
-              <PrismicRichText field={data.peripheralTitle} />
+        <div className="text-white-100 grid grid-cols-1 gap-16 md:grid-cols-12 md:gap-8">
+          <div className="col-span-4">
+            <div className="font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-neutral-100 dark:text-blue-100">
+              {data.peripheralTitle}
             </div>
-            <div className="pb-20 font-charmRegular text-pLGRegular font-normal tracking-[0.02em] text-lightestGrey-100">
-              {asText(data.peripheralDescription)}
+            <div className="pt-6 pb-20 font-ABCWhyteEdu_Regular text-pLGRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+              {data.peripheralDescription}
             </div>
           </div>
         </div>
@@ -113,45 +115,53 @@ const Gear: FC<IGear> = ({ data, settings }) => (
                     layout="fixed"
                     quality={100}
                   />
-                  <div className="max-w-[16.5rem] pt-8 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100 ">
-                    <PrismicRichText field={peripheralDetails} />
+                  <div className="pt-8 font-ABCWhyteEdu_Regular text-pMDRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+                    {peripheralDetails}
                   </div>
                 </div>
               )
             )}
           </div>
         </div>
-        <div className="font-charmSemiBold text-pLGSemiBold font-normal tracking-[0.02em]  text-purple-100">
-          {asText(data.softwareTitle)}
-        </div>
-        <div className="pb-20 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100">
-          <PrismicRichText field={data.softwareTitleDescription} />
-        </div>
-        <div>
-          {data.software.map(
-            ({ softwareTitle, softwareDescription, softwareIcon }, index) => (
-              <div key={index}>
-                <div className="flex pb-12">
-                  <Image
-                    src={softwareIcon.url ?? ''}
-                    alt={softwareIcon.alt ?? ''}
-                    width={24}
-                    height={24}
-                    layout="fixed"
-                    quality={100}
-                  />
-                  <div className="text-left">
-                    <div className="pl-2 pb-4 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100">
-                      <PrismicRichText field={softwareTitle} />
-                    </div>
-                    <div className="pl-2 pb-4 font-charmSemiBold text-pMDSemiBold font-semibold tracking-[0.02em] text-white-100">
-                      <PrismicRichText field={softwareDescription} />
+        <div className="grid grid-cols-12 gap-16">
+          <div className="col-span-7 col-start-1">
+            <div className="font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-neutral-100 dark:text-blue-100">
+              {data.softwareTitle}
+            </div>
+            <div className="font-charmSemiBold text-white-100 pb-20 text-pMDSemiBold font-semibold tracking-[0.02em]">
+              {data.softwareTitleDescription}
+            </div>
+            <div>
+              {data.software.map(
+                (
+                  { softwareTitle, softwareDescription, softwareIcon },
+                  index
+                ) => (
+                  <div key={index}>
+                    <div className="col-span-6 col-start-1 block pb-12">
+                      <div className="flex">
+                        <Image
+                          src={softwareIcon.url ?? ''}
+                          alt={softwareIcon.alt ?? ''}
+                          width={24}
+                          height={24}
+                          layout="fixed"
+                          quality={100}
+                        />
+
+                        <div className="pl-2 font-ABCWhyteEdu_Medium text-pLGSemiBold font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-0">
+                          {softwareTitle}
+                        </div>
+                      </div>
+                      <div className="pt-4 font-ABCWhyteEdu_Regular text-pLGRegular font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-15">
+                        {softwareDescription}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            )
-          )}
+                )
+              )}
+            </div>
+          </div>
         </div>
       </Container>
     </div>
