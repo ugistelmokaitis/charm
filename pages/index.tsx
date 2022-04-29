d29d4d0c69002b5ed36ca21454ab3e9851498008
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-04-29 12:40:04 +1000

    Update[index.tsx]: add dark mode & fix spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import type { GetStaticProps } from 'next';
 import type { FC } from 'react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import type { JSXMapSerializer } from '@prismicio/react';
 import { PrismicRichText } from '@prismicio/react';
 import { asText } from '@prismicio/helpers';
-import { useWindowScroll } from 'react-use';
+import { useWindowScroll, useWindowSize } from 'react-use';
+import { useMeasure } from '@react-hookz/web';
 import Layout from '../components/layout';
-import { getPage, getPages } from '../utils/prismic';
+import { docResolver, getPage, getPages, linkResolver } from '../utils/prismic';
 import type { HomeProps } from '../types/home';
 import type { SettingsProps } from '../types/settings';
 import Container from '../components/container';
 import BadgeButton from '../components/badgeButton';
-import Window from '../components/window';
+import EditorWindow from '../components/editorWindow';
 import richTextComponents from '../components/richTextComponents';
 
 type IHome = {
@@ -24,7 +25,7 @@ const sliderComponents: JSXMapSerializer = {
   heading3: ({ children, key }) => (
     <h3
       key={key}
-      className="whitespace-nowrap font-ABCWhyteEdu_Heavy text-header3 font-extrabold"
+      className="whitespace-nowrap font-ABCWhyteEdu_Heavy text-header3 font-extrabold text-neutral-30"
     >
       {children}
     </h3>
@@ -36,40 +37,41 @@ const Home: FC<IHome> = ({ data, settings }) => {
   const half = Math.ceil(firstHalf.length / 2);
   const secondHalf = firstHalf.splice(0, half);
   const [activeExperience, setActiveExperience] = useState(0);
-
+  const slider = useRef<HTMLDivElement>(null);
   const { y: scrollY } = useWindowScroll();
-
-  console.log(activeExperience, 'ac');
-
+  const scrollPos = slider.current?.offsetTop ?? 0;
+  const windowSize = useWindowSize();
+  const diff = (scrollY - (scrollPos - windowSize.height / 2)) / 2;
   return (
     <Layout title="" description="" settings={settings}>
       <div className="flex bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100">
         <Container>
           <div className="grid-starts-1 grid grid-cols-12 gap-16">
             <div className="col-span-8 col-start-3 pt-32 text-center">
-              <BadgeButton />
+              <BadgeButton
+                href={`${docResolver(data.badgeButtonLink)}`}
+                child1={data.badgeButtonPrefix}
+                child2={data.badgeButtonLabel}
+              />
 
-              <div className="mx-auto items-center pt-12 font-ABCWhyteEdu_Heavy text-display font-extrabold text-neutral-100">
-                {asText(data.heroGreetingTitle)}
+              <div className="mx-auto items-center pt-12 font-ABCWhyteEdu_Heavy text-display font-extrabold text-neutral-100 dark:text-neutral-0">
+                {data.heroGreetingTitle}
               </div>
-              <div className="mx-auto mt-4 max-w-[60rem] items-center pb-12 font-ABCWhyteEdu_Heavy text-display font-extrabold text-neutral-100">
-                {asText(data.heroNameTitle)}
+              <div className="mx-auto mt-4 max-w-[60rem] items-center pb-12 font-ABCWhyteEdu_Heavy text-display font-extrabold text-neutral-100 dark:text-neutral-0">
+                {data.heroNameTitle}
               </div>
-              <div className="mx-auto max-w-[39rem] pb-12 font-ABCWhyteEdu_Medium text-pMDSemiBold font-semibold tracking-[0.02em] text-neutral-50">
-                {asText(data.heroDescription)}
+              <div className="mx-auto max-w-[39rem] font-ABCWhyteEdu_Medium text-pMDSemiBold font-semibold tracking-[0.02em] text-neutral-50 dark:text-neutral-15">
+                {data.heroDescription}
               </div>
             </div>
           </div>
 
-          <Window />
+          <EditorWindow />
           <div className="mt-52">
             <PrismicRichText field={data.experienceTitle} />
           </div>
-          <div className="mt-20 grid ">
-            <div className="font-ABCWhyteEdu_Heavy text-header1 font-extrabold text-neutral-100">
-              Experience
-            </div>
-            <div className="mt-20 grid grid-cols-12 gap-16">
+          <div className="mt-28 grid ">
+            <div className="grid grid-cols-12 gap-16">
               <div className="col-span-4 col-start-1 block">
                 {data.company.map(({ companyRole, companyName }, index) => (
                   <div
@@ -80,66 +82,73 @@ const Home: FC<IHome> = ({ data, settings }) => {
                     tabIndex={0}
                     className={` ${
                       activeExperience === index
-                        ? 'bg-primary-5'
+                        ? 'rounded-lg bg-primary-5'
                         : 'bg-neutral-0'
                     }`}
                   >
-                    <div className="font-FiraCode_SemiBold text-codeLGSemiBold font-semibold text-neutral-100">
-                      {companyName}
-                    </div>
-                    <div className="font-codeRegular text-neutral-100-Code font-FiraCode_Regular text-codeMDRegular text-neutral-100">
-                      {companyRole}
+                    <div className="px-8 py-8">
+                      <div className="font-FiraCode_SemiBold text-codeLGSemiBold font-semibold text-neutral-100">
+                        {companyName}
+                      </div>
+                      <div className="font-codeRegular mt-2 font-FiraCode_Regular text-codeMDRegular text-neutral-100">
+                        {companyRole}
+                      </div>
                     </div>
                   </div>
                 ))}
               </div>
               <div className="col-span-6 col-start-7">
-                <div className="text-neutral-100-Code mb-5 font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-neutral-100">
+                <div className="pb-4 font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-primary-100">
                   {data.company[activeExperience].companyYear}
                 </div>
-                <div className="font-ABCWhyteEdu_Medium text-header4 font-semibold tracking-[0.02em] text-neutral-100">
-                  {data.company[activeExperience].companyResponsibilities}
-                </div>{' '}
-                <div>{data.company[activeExperience].companyTools}</div>
+                <div className="inline-block">
+                  <span className="font-ABCWhyteEdu_Medium text-header4 font-semibold tracking-[0.02em] text-neutral-100">
+                    {data.company[activeExperience].companyResponsibilities}
+
+                    <span className="ml-2 font-ABCWhyteEdu_Medium text-header4 font-semibold tracking-[0.02em] text-neutral-30">
+                      {data.company[activeExperience].companyTools}
+                    </span>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </Container>
       </div>
 
-      <div className="mt-72 block overflow-hidden bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 ">
-        <div>
-          <div className="mb-28 flex items-center justify-center">
-            <PrismicRichText field={data.skillsTitle} />
-          </div>
-          <div className="flex text-neutral-30">
-            {firstHalf.map(({ skill }, index) => (
-              <div
-                key={index}
-                className="flex pr-4"
-                style={{ transform: `translateX(${scrollY / 3}px)` }}
-              >
-                <PrismicRichText field={skill} components={sliderComponents} />
-              </div>
-            ))}
-          </div>
+      <div
+        className="block overflow-hidden bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100"
+        ref={slider}
+      >
+        <div className="mx-auto mt-52 flex max-w-[35rem] items-center justify-center pb-28 text-center">
+          <PrismicRichText field={data.skillsTitle} />
         </div>
-
-        <div>
-          <div className="flex text-neutral-30">
-            {secondHalf.map(({ skill }, index) => (
-              <div
-                key={index}
-                className="pr-4"
-                style={{ transform: `translateX(-${scrollY / 2}px)` }}
-              >
-                <PrismicRichText field={skill} key={index} />
-              </div>
-            ))}
-          </div>
+        <div
+          className="flex text-neutral-30"
+          style={{ transform: `translateX(${diff - windowSize.width / 7}px)` }}
+        >
+          {firstHalf.map(({ skill }, index) => (
+            <div key={index} className="flex pr-4 pb-8">
+              <PrismicRichText field={skill} components={sliderComponents} />
+            </div>
+          ))}
         </div>
-
-        <div className="pb-52" />
+        <div
+          className="text-neutral-40 flex"
+          style={{
+            transform: `translateX(${-diff - windowSize.width / 7}px)`,
+          }}
+        >
+          {secondHalf.map(({ skill }, index) => (
+            <div key={index} className="pr-4">
+              <PrismicRichText
+                field={skill}
+                key={index}
+                components={sliderComponents}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   );
