8fbed20cafde7cf5f8bf7d4a0359b867e5323d0e
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-04-29 12:34:42 +1000

    Update[divider component]: add dark mode css

--- components/divider.tsx
+++ components/divider.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
 
-const Divider: FC = () => <hr className="border-mediumGrey-100 border-t " />;
+const Divider: FC = () => (
+  <hr className="border-t border-neutral-15 dark:border-neutral-50" />
+);
 
 export default Divider;
