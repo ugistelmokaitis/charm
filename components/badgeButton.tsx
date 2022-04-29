8b51dab0f26a033daddd2b5228bc0e4fc8030fde
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-04-29 12:32:06 +1000

    Update[badgeButton component]: add children & dark mode css

--- components/badgeButton.tsx
+++ components/badgeButton.tsx
@@ -1,16 +1,27 @@
 import type { FC } from 'react';
+import type React from 'react';
+import type { LinkProps } from 'next/link';
 import RightArrowIcon from '../public/icons/rightArrowIcon.svg';
 
-const TagButton: FC = () => (
-  <div className="inline-flex items-center justify-between rounded-full bg-primary-25 py-1 px-1 pr-4 hover:bg-primary-50">
+type BadgeButtonProps = LinkProps & {
+  href: string;
+  child1: React.ReactNode;
+  child2: React.ReactNode;
+};
+
+const TagButton: FC<BadgeButtonProps> = ({ child1, child2, ...props }) => (
+  <a
+    className="inline-flex items-center justify-between rounded-full bg-primary-25 py-1 px-1 pr-4 hover:bg-primary-50 dark:bg-neutral-65 dark:hover:bg-neutral-50"
+    {...props}
+  >
     <span className="mr-3 rounded-full bg-primary-100 px-4 py-1 text-neutral-0">
-      New
+      {child1}
     </span>
-    <span className="text-neutral-100">Im Opened to Work!</span>
+    <span className="text-neutral-100 dark:text-neutral-0">{child2}</span>
     <div className="ml-4">
       <RightArrowIcon />
     </div>
-  </div>
+  </a>
 );
 
 export default TagButton;
