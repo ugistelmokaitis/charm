97ad2dab220de0d1049442a09c5c2dd264064de1
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-05-02 17:02:35 +1000

    Update[casestudy ts]: add types for the casestudy

--- types/casestudy.ts
+++ types/casestudy.ts
@@ -3,10 +3,20 @@ import type {
   PrismicDocumentWithUID,
   RichTextField,
   SliceZone,
+  GroupField,
 } from '@prismicio/types';
 
 export type CaseStudyProps = PrismicDocumentWithUID<{
   contentPrefix: KeyTextField;
   contentTitle: RichTextField;
   slices1: SliceZone;
+  content: ContentProps;
+}>;
+
+type ContentProps = GroupField<{
+  prefix: KeyTextField;
+  title: RichTextField;
+  description: KeyTextField;
+  primaryButtonLabel: KeyTextField;
+  primaryButtonLink: KeyTextField;
 }>;
