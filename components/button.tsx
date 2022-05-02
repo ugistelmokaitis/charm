e2c5f363dd3a9c2ddb8ec842431465fbe74604e5
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-05-02 15:18:27 +1000

    Update[button component]: add 'neutral variant' for navigation & footer

--- components/button.tsx
+++ components/button.tsx
@@ -8,15 +8,16 @@ type ButtonProps = LinkProps & {
   className?: string;
   Icon?: SvgrComponent;
   children: React.ReactNode;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'neutral';
 };
 
 const classes = {
   variant: {
     primary:
-      'bg-primary-100 text-neutral-0 outline-[0.0625rem] active:outline-[0.125rem] disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none active:outline-primary-50 outline-none',
+      'rounded py-[0.7188rem] px-[1.8175rem] bg-primary-100 text-neutral-0 outline-[0.0625rem] active:outline-[0.125rem] disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none active:outline-primary-50 outline-none',
     secondary:
-      'outline outline-[0.0625rem] hover:bg-primary-25 outline-neutral-65 active:outline-[0.125rem] active:outline-primary-200 disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none dark:bg-neutral-100 dark:hover:bg-neutral-65 dark:text-neutral-0 dark:disabled:bg-neutral-80 dark:disabled:text-neutral-50 dark:disabled:hover:outline-none dark:active:outline-neutral-15 dark:active:bg-neutral-65',
+      'rounded py-[0.7188rem] px-[1.8175rem] outline outline-[0.0625rem] hover:bg-primary-25 outline-neutral-65 active:outline-[0.125rem] active:outline-primary-200 disabled:bg-primary-5 disabled:text-neutral-30 disabled:hover:outline-none dark:bg-neutral-100 dark:hover:bg-neutral-65 dark:text-neutral-0 dark:disabled:bg-neutral-80 dark:disabled:text-neutral-50 dark:disabled:hover:outline-none dark:active:outline-neutral-15 dark:active:bg-neutral-65',
+    neutral: 'text-neutral-50 dark:text-neutral-30',
   },
 };
 
@@ -28,7 +29,7 @@ const Button: FC<ButtonProps> = ({
   ...props
 }) => (
   <a
-    className={`flex items-center justify-center rounded py-[0.7188rem] px-[1.8175rem]
+    className={`flex items-center justify-center
     ${classes.variant[variant]} ${className}`}
     {...props}
   >
