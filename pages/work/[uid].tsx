de5868bbd70aa3d03385ff1c0d0beca13dc51302
Ugi Stelmokaitis <hello@ugistelmokaitis.com>
2022-05-08 21:50:40 +1000

    Update[work uid]: slicezone add to casestudy

--- pages/work/[uid].tsx
+++ pages/work/[uid].tsx
@@ -6,28 +6,51 @@ import type {
   KeyTextField,
   PrismicDocumentWithUID,
 } from '@prismicio/types';
-import type { SliceZoneComponents, SliceZoneProps } from '@prismicio/react';
-import { SliceZone } from '@prismicio/react';
+import type { SliceZoneProps, SliceZoneComponents } from '@prismicio/react';
+import { PrismicImage, PrismicRichText, SliceZone } from '@prismicio/react';
 import Image from 'next/image';
 import Layout from '../../components/layout';
 import { getPage, getPages } from '../../utils/prismic';
 import { components } from '../../slices';
 import type { SettingsProps } from '../../types/settings';
 import type { CaseStudyProps } from '../../types/casestudy';
+import Container from '../../components/container';
 
 type ICaseStudy = {
   settings: SettingsProps;
   data: CaseStudyProps['data'];
-  slices1: SliceZoneProps['slices'];
+  slices2: SliceZoneProps['slices'];
 };
 
 const CaseStudy: FC<ICaseStudy> = ({ data, settings }) => (
   <Layout title="" description="" settings={settings}>
-    <div className="flex flex-col gap-8">
-      <SliceZone
-        slices={data.slices1}
-        components={components as unknown as SliceZoneComponents}
-      />
+    <div className="flex bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100">
+      <Container>
+        <div className="flex bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100">
+          <div className="grid-starts-1 text-white-100 grid grid-cols-12 gap-16 pt-32 md:grid-cols-12 md:gap-8">
+            <div className="col-span-6">
+              <div className="mb-8 font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-primary-100 dark:text-blue-100">
+                {data.contentPrefix}
+              </div>
+              <PrismicRichText field={data.contentTitle} />
+            </div>
+            <div className="col-span-12 mt-28 mb-[8rem]">
+              <Image
+                src={data.contentImage.url ?? ''}
+                alt={data.contentImage.alt ?? ''}
+                width={1248}
+                height={650}
+                layout="responsive"
+                quality={100}
+              />
+            </div>
+            <SliceZone
+              slices={data.slices2}
+              components={components as unknown as SliceZoneComponents}
+            />
+          </div>
+        </div>
+      </Container>
     </div>
   </Layout>
 );
