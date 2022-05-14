954127e4bb803790e3a49318d679711d9acffec3
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-05-14 14:56:12 +1000

    Update[work page]: add mobile responsive

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -21,14 +21,13 @@ const Work: FC<IWork> = ({ data, settings, casestudies }) => (
   <Layout title="" description="" settings={settings}>
     <div className="flex bg-neutral-0 selection:bg-primary-50 selection:text-neutral-100 dark:bg-neutral-100">
       <Container>
-        <div className="grid-starts-1 text-white-100 grid grid-cols-12 gap-16  pt-32 md:grid-cols-12 md:gap-8">
+        <div className="grid-starts-1 gap-16 pt-56 md:grid-cols-12 md:gap-8 lg:grid">
           <div className="col-span-4">
             <PrismicRichText field={data.introTitle} />
-            <div className="mb-28 mt-28" />
           </div>
         </div>
         {casestudies.map((casestudy, index) => (
-          <div key={index}>
+          <div key={index} className="pt-20 lg:pt-28">
             <PrismicLink document={casestudy}>
               <Image
                 src={casestudy.data.contentImage.url ?? ''}
@@ -39,21 +38,21 @@ const Work: FC<IWork> = ({ data, settings, casestudies }) => (
                 quality={100}
               />
             </PrismicLink>
-            <div className="mt-20 grid grid-cols-12  gap-16 md:grid-cols-12 md:gap-8">
+            <div className="mt-12 gap-16 md:grid-cols-12 lg:grid">
               <div className="col-span-6 col-start-3">
                 <div className="mb-8 font-FiraCode_SemiBold text-codeMDSemiBold font-semibold text-primary-100 dark:text-blue-100">
                   {casestudy.data.workpagePrefix}
                 </div>
                 <PrismicLink document={casestudy}>
-                  <div className="font-ABCWhyteEdu_Heavy text-header2 font-extrabold text-neutral-100 hover:text-neutral-50 dark:text-neutral-0 dark:hover:text-neutral-30">
+                  <div className="font-ABCWhyteEdu_Heavy text-5xl font-bold tracking-[0.02em] text-neutral-100 hover:text-neutral-50 dark:text-neutral-0 dark:hover:text-neutral-30 lg:text-3xl">
                     {casestudy.data.workpageTitle}
                   </div>
                 </PrismicLink>
-                <div className="mb-40 mt-12 inline-block">
-                  <span className="font-ABCWhyteEdu_Medium text-header4 font-semibold tracking-[0.02em] text-neutral-100 dark:text-neutral-0">
+                <div className="mt-8 inline-block lg:mt-12">
+                  <span className="font-ABCWhyteEdu_Medium  text-pSMSemiBold font-normal tracking-[0.02em] text-neutral-100 dark:text-neutral-0 lg:text-pLGSemiBold">
                     {casestudy.data.workpageDescriptionBold}
 
-                    <span className="ml-2 font-ABCWhyteEdu_Medium text-header4 font-semibold tracking-[0.02em] text-neutral-30 dark:text-neutral-30">
+                    <span className=" text-neutral-30 dark:text-neutral-30">
                       {casestudy.data.workpageDescriptionRegular}
                     </span>
                   </span>
