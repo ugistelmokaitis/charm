c5c4084a69382f527df99f21c3c72aba26d5cfd0
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-03 14:15:16 +1000

    Update[eslint]: fix eslint config file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-    plugins: ['stylelint-prettier'],
-    extends: [
-      'stylelint-config-standard',
-      'stylelint-config-idiomatic-order',
-      'stylelint-config-prettier',
+  plugins: ["stylelint-prettier"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-idiomatic-order",
+    "stylelint-config-prettier",
+  ],
+  rules: {
+    "prettier/prettier": true,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "layer",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
     ],
-    rules: {
-      'prettier/prettier': true,
-      'at-rule-no-unknown': [
-        true,
-        {
-          ignoreAtRules: [
-            'tailwind',
-            'apply',
-            'layer',
-            'variants',
-            'responsive',
-            'screen',
-          ],
-        },
-      ],
-      'declaration-block-trailing-semicolon': null,
-      'no-descending-specificity': null,
-      'declaration-block-no-redundant-longhand-properties': [
-        true,
-        {
-          ignoreShorthands: ['/flex/'],
-        },
-      ],
-      'selector-pseudo-class-no-unknown': [
-        true,
-        {
-          ignorePseudoClasses: ['global'],
-        },
-      ],
-    },
-  };
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null,
+    "declaration-block-no-redundant-longhand-properties": [
+      true,
+      {
+        ignoreShorthands: ["/flex/"],
+      },
+    ],
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+  },
+};
