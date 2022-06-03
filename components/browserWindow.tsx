ac33e015a4816624980f4b8e3402bb91fc85f080
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-04 03:42:06 +1000

    Update[browser window]: add svg icon

--- components/browserWindow.tsx
+++ components/browserWindow.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react';
 import type React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { hybrid, xcode } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import TypescriptLogo from '../public/icons/typescriptlogo.svg';
 
 type IBrowseWindow = {
   child1: React.ReactNode;
@@ -12,13 +13,14 @@ const BrowserWindow: FC<IBrowseWindow> = ({ child1, child2 }) => (
   <>
     <div className="hidden dark:flex">
       <div className="container dark:drop-shadow-[0px_-10px_200px_rgba(119,197,227,0.50)]">
-        <div className="grid-starts-1 gap-16 pt-20 md:grid-cols-12 lg:grid">
+        <div className="grid-starts-1 mt-8 gap-16 md:grid-cols-12 lg:grid">
           <div className="col-span-10 col-start-2 my-12">
             <div className="mx-auto">
-              <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-80 bg-[#0B0B0B]/80 px-4">
+              <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-80 bg-[#0B0B0B]/80 px-5">
                 <div className="-mb-[1.7px] -ml-2 border-t-[1px] border-yellow-100/60">
-                  <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-65 pr-[0.375rem]">
-                    <span className="font-codeRegular font-FiraCode-Regular text-[0.875rem] leading-[1.25rem]  text-neutral-30">
+                  <div className="item flex h-[2.7375rem] items-center justify-center border-r border-neutral-65 pr-[0.375rem]">
+                    <TypescriptLogo className="h-4 w-4 text-neutral-50" />
+                    <span className="font-codeRegular pl-2 font-FiraCode-Regular text-[0.875rem] leading-[1.25rem]  text-neutral-30">
                       {child1}
                     </span>
                     <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-30" />
@@ -49,13 +51,14 @@ const BrowserWindow: FC<IBrowseWindow> = ({ child1, child2 }) => (
     <div>
       <div className="flex dark:hidden">
         <div className="container drop-shadow-[0px_-10px_200px_rgba(17,24,39,0.25)]">
-          <div className="grid-starts-1 grid-cols-12 gap-16 pt-20 lg:grid">
+          <div className="grid-starts-1 mt-8 grid-cols-12 gap-16 lg:grid">
             <div className="col-span-10 col-start-2 my-12">
               <div className="mx-auto">
-                <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-15 bg-primary-50 px-4">
+                <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-15 bg-primary-50 px-5">
                   <div className="-mb-[1.7px] -ml-2 border-t-[1px] border-primary-75">
                     <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-30/30 pr-[0.375rem]">
-                      <span className="font-codeRegular font-FiraCode-Regular text-[0.875rem] leading-[1.25rem] text-neutral-65">
+                      <TypescriptLogo className="h-4 w-4 text-neutral-65" />
+                      <span className="font-codeRegular  pl-2 font-FiraCode-Regular text-[0.875rem] leading-[1.25rem] text-neutral-65">
                         {child1}
                       </span>
                       <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-50" />
