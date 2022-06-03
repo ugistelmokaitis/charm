211265e66de4e464c03ca5aea975e3f5de49c1ca
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-03 20:33:40 +1000

    Update[browser window component]: fix font size, add css to look better window

--- components/browserWindow.tsx
+++ components/browserWindow.tsx
@@ -15,12 +15,14 @@ const BrowserWindow: FC<IBrowseWindow> = ({ child1, child2 }) => (
         <div className="grid-starts-1 gap-16 pt-20 md:grid-cols-12 lg:grid">
           <div className="col-span-10 col-start-2 my-12">
             <div className="mx-auto">
-              <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg bg-[#0B0B0B] px-3 ">
-                <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-50 pr-[0.375rem]">
-                  <span className="font-codeRegular font-FiraCode-Regular text-codeMDRegular leading-[4rem] text-neutral-30">
-                    {child1}
-                  </span>
-                  <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-30" />
+              <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-80 bg-[#0B0B0B]/80 px-4">
+                <div className="-mb-[1.7px] -ml-2 border-t-[1px] border-yellow-100/60">
+                  <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-65 pr-[0.375rem]">
+                    <span className="font-codeRegular font-FiraCode-Regular text-[0.875rem] leading-[1.25rem]  text-neutral-30">
+                      {child1}
+                    </span>
+                    <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-30" />
+                  </div>
                 </div>
               </div>
               <SyntaxHighlighter
@@ -29,7 +31,7 @@ const BrowserWindow: FC<IBrowseWindow> = ({ child1, child2 }) => (
                 showLineNumbers
                 style={hybrid}
                 language="typescript"
-                className="rounded-b-lg"
+                className="rounded-b-lg border-[0.0625rem] border-neutral-80/60"
                 customStyle={{
                   paddingTop: '3rem',
                   paddingBottom: '3rem',
@@ -50,20 +52,23 @@ const BrowserWindow: FC<IBrowseWindow> = ({ child1, child2 }) => (
           <div className="grid-starts-1 grid-cols-12 gap-16 pt-20 lg:grid">
             <div className="col-span-10 col-start-2 my-12">
               <div className="mx-auto">
-                <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg bg-primary-50 px-3 ">
-                  <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-50 pr-[0.375rem]">
-                    <span className="font-codeRegular font-FiraCode-Regular text-codeMDRegular leading-[4rem] text-neutral-65">
-                      {child1}
-                    </span>
-                    <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-50" />
+                <div className="flex h-11 w-full items-center justify-start space-x-1.5 rounded-t-lg border-[1px] border-neutral-15 bg-primary-50 px-4">
+                  <div className="-mb-[1.7px] -ml-2 border-t-[1px] border-primary-75">
+                    <div className="flex h-[2.7375rem] items-center justify-center border-r border-neutral-30/30 pr-[0.375rem]">
+                      <span className="font-codeRegular font-FiraCode-Regular text-[0.875rem] leading-[1.25rem] text-neutral-65">
+                        {child1}
+                      </span>
+                      <div className="ml-2 mr-2 h-[0.4063rem] w-[0.4063rem] rounded-full border-neutral-0 bg-neutral-50" />
+                    </div>
                   </div>
                 </div>
                 <SyntaxHighlighter
                   wrapLongLines
                   wrapLines
+                  language="typescript"
                   showLineNumbers
                   style={xcode}
-                  className="rounded-b-lg"
+                  className="rounded-b-lg border-[0.0625rem] border-neutral-15/60"
                   customStyle={{
                     paddingTop: '3rem',
                     paddingBottom: '3rem',
