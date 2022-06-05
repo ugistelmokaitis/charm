c49ddcef3d2139b290352155ef0be17ee4ae8a53
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-05 22:39:19 +1000

    Remove[geometry]: change slice with media player

--- .slicemachine/assets/slices/Geometry/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Geometry';
-
-export default {
-  title: 'slices/Geometry'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"geometry","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1571126770897-2d612d1f7b89?w=900&h=500&fit=crop"}},"id":"_Default"}} />
-_Default.storyName = 'Default'
