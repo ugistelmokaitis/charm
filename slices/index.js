c73d254ee6c5dc817e8d7d76b6ff9d20e6116aa9
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-05 22:42:16 +1000

    Update[slice import & remove geometry]: update default slice import file

--- slices/index.js
+++ slices/index.js
@@ -2,28 +2,46 @@
 
 import Callout from './Callout';
 import Carousel from './Carousel';
+import CodeBlock from './CodeBlock';
 import Content from './Content';
 import Fullwidthimage from './Fullwidthimage';
 import Keywords from './Keywords';
 import Masonrygrid from './Masonrygrid';
+import MediaPlayer from './MediaPlayer';
 import Quote from './Quote';
+import RegularImage from './RegularImage';
+import RichText from './RichText';
+import TitleText from './TitleText';
+import Twitter from './Twitter';
 
 export {
 	Callout,
 	Carousel,
+	CodeBlock,
 	Content,
 	Fullwidthimage,
 	Keywords,
 	Masonrygrid,
+	MediaPlayer,
 	Quote,
+	RegularImage,
+	RichText,
+	TitleText,
+	Twitter,
 };
 
 export const components = {
 	callout: Callout,
 	carousel: Carousel,
+	code_block: CodeBlock,
 	content: Content,
 	fullwidthimage: Fullwidthimage,
 	keywords: Keywords,
 	masonrygrid: Masonrygrid,
+	media_player: MediaPlayer,
 	quote: Quote,
+	regular_image: RegularImage,
+	rich_text: RichText,
+	title_text: TitleText,
+	twitter: Twitter,
 };
