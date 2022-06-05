f1f5e1c123bb73d3d428895d8590e616aa1691b6
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-05 22:40:30 +1000

    Add[[richtext slice]: add blog slice

---
+++ .slicemachine/assets/slices/RichText/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/RichText';
+
+export default {
+  title: 'slices/RichText'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"rich_text","items":[],"primary":{"richtext":[{"type":"paragraph","text":"Irure eu et dolore dolor eu incididunt in voluptate commodo.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
