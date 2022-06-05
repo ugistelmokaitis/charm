96f7f5ed6adca7baa6f4fc9b481f25d61ea5f0f3
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-05 22:43:29 +1000

    Add[slicemachine files]: general slicmachine files

---
+++ .slicemachine/assets/slices/Twitter/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Twitter';
+
+export default {
+  title: 'slices/Twitter'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"twitter","items":[],"primary":{"tweetId":"aggregate robust paradigms"},"id":"_Default"}} />
+_Default.storyName = 'Default'
