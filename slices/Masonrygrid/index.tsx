6e19ff788cc7f2fd8c7a385d5a03214f00e1a483
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-06-11 22:43:34 +1000

    Update[masonry grid]: mobile responsive  & changes size of image

--- slices/Masonrygrid/index.tsx
+++ slices/Masonrygrid/index.tsx
@@ -1,5 +1,4 @@
 import type { FC } from 'react';
-import React, { Fragment } from 'react';
 import type { SliceComponentProps } from '@prismicio/react';
 import type { ImageField } from '@prismicio/types';
 import Image from 'next/image';
@@ -15,18 +14,19 @@ const Masonrygrid: FC<
     };
   }>
 > = ({ slice }) => (
-  <div className="container col-span-12  col-start-1 mt-20 mb-20 ">
+  <div className="container col-span-8  col-start-3 mt-8 sm:mt-28 lg:mt-16">
     <div className="masonry sm:masonry-sm md:masonry-md">
-      <div className="break-inside pb-8">
+      <div className="break-inside pb-5">
         <div className="card-zoom">
           <div className="card-zoom-image">
             <Image
               src={slice.primary.imageOne.url ?? ''}
               alt={slice.primary.imageOne.alt ?? ''}
-              width={614}
-              height={650}
+              width={403}
+              height={480}
               layout="responsive"
               quality={100}
+              priority
             />
           </div>
         </div>
@@ -37,37 +37,40 @@ const Masonrygrid: FC<
             <Image
               src={slice.primary.imageTwo.url ?? ''}
               alt={slice.primary.imageTwo.alt ?? ''}
-              width={614}
-              height={650}
+              width={403}
+              height={480}
               layout="responsive"
               quality={100}
+              priority
             />
           </div>
         </div>
       </div>
-      <div className="break-inside pt-40">
+      <div className="break-inside pt-20">
         <div className="card-zoom">
           <div className="card-zoom-image">
             <Image
               src={slice.primary.imageThree.url ?? ''}
               alt={slice.primary.imageThree.alt ?? ''}
-              width={614}
-              height={650}
+              width={403}
+              height={480}
               layout="responsive"
               quality={100}
+              priority
             />
           </div>
         </div>
-        <div className="break-inside pt-8">
+        <div className="break-inside pt-5">
           <div className="card-zoom">
             <div className="card-zoom-image">
               <Image
                 src={slice.primary.imageFour.url ?? ''}
                 alt={slice.primary.imageFour.alt ?? ''}
-                width={614}
-                height={650}
+                width={403}
+                height={480}
                 layout="responsive"
                 quality={100}
+                priority
               />
             </div>
           </div>
