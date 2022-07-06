2c9530aff713f4ab746b953215381e2a3e3c622b
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-07-06 16:00:36 +1000

    Update[case study page]: sort case studies by completion date

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps } from 'next';
 import type { FC } from 'react';
 import { PrismicLink, PrismicRichText } from '@prismicio/react';
 import Image from 'next/image';
+import { parse } from 'date-fns';
 import Layout from '../../components/layout';
 import { getPage, getPages } from '../../utils/prismic';
 import type { WorkProps } from '../../types/work';
@@ -31,34 +32,49 @@ const Work: FC<IWork> = ({ data, settings, casestudies }) => (
     <div>
       <Container>
         <div className="grid grid-cols-1 gap-y-8 gap-x-4 sm:grid-cols-2 sm:gap-y-12">
-          {casestudies.map((casestudy, index) => (
-            <div
-              key={index}
-              className="group rounded-lg p-4 hover:bg-neutral-0 hover:shadow-sm dark:hover:bg-neutral-80"
-            >
-              <PrismicLink document={casestudy}>
-                <div className="overflow-hidden rounded-t-xl rounded-b-xl">
-                  <Image
-                    src={casestudy.data.contentImage.url ?? ''}
-                    alt={casestudy.data.contentImage.alt ?? ''}
-                    width={576}
-                    height={360}
-                    layout="responsive"
-                    quality={100}
-                    priority
-                  />
-                </div>
-                <h2 className="ABCWhyteEdu-Medium mt-6 flex items-center align-middle text-pm2 font-medium text-neutral-100 dark:text-neutral-0 sm:text-pm1">
-                  {casestudy.data.companyName}
-                </h2>
-                <div className="mt-2 flex items-center">
-                  <p className=" ABCWhyteEdu-Book text-pm3 font-[350] text-neutral-65 dark:text-neutral-15 sm:text-pm2">
-                    {casestudy.data.aboutCompany}
-                  </p>
-                </div>
-              </PrismicLink>
-            </div>
-          ))}
+          {casestudies
+            .sort((caseStudyA, caseStudyB) => {
+              const caseStudyADate = parse(
+                caseStudyA.data.date,
+                'yyyy-MM-dd',
+                new Date()
+              );
+              const caseStudyBDate = parse(
+                caseStudyB.data.date,
+                'yyyy-MM-dd',
+                new Date()
+              );
+
+              return caseStudyADate > caseStudyBDate ? -1 : 1;
+            })
+            .map((casestudy, index) => (
+              <div
+                key={index}
+                className="group rounded-lg p-4 hover:bg-neutral-0 hover:shadow-sm dark:hover:bg-neutral-80"
+              >
+                <PrismicLink document={casestudy}>
+                  <div className="overflow-hidden rounded-t-xl rounded-b-xl">
+                    <Image
+                      src={casestudy.data.contentImage.url ?? ''}
+                      alt={casestudy.data.contentImage.alt ?? ''}
+                      width={576}
+                      height={360}
+                      layout="responsive"
+                      quality={100}
+                      priority
+                    />
+                  </div>
+                  <h2 className="ABCWhyteEdu-Medium mt-6 flex items-center align-middle text-pm2 font-medium text-neutral-100 dark:text-neutral-0 sm:text-pm1">
+                    {casestudy.data.companyName}
+                  </h2>
+                  <div className="mt-2 flex items-center">
+                    <p className=" ABCWhyteEdu-Book text-pm3 font-[350] text-neutral-65 dark:text-neutral-15 sm:text-pm2">
+                      {casestudy.data.aboutCompany}
+                    </p>
+                  </div>
+                </PrismicLink>
+              </div>
+            ))}
         </div>
       </Container>
     </div>
