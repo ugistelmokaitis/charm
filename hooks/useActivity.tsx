e9282f708bf868ff521f66d7e90b3b1181d24a4b
ugistelmokaitis <hello@ugistelmokaitis.com>
2022-07-14 14:55:41 +1000

    Update[activity component]: comment it out code for now as not used

--- hooks/useActivity.tsx
+++ hooks/useActivity.tsx
@@ -6,17 +6,21 @@ type ActivityResponse = {
   status: string;
 };
 
-type GitHubResponse = {
-  active: boolean;
-};
+/*
+ * type GitHubResponse = {
+ *   active: boolean;
+ * };
+ */
 
 type VercelResponse = {
   active: boolean;
 };
 
-type TwitterResponse = {
-  active: boolean;
-};
+/*
+ * type TwitterResponse = {
+ *   active: boolean;
+ * };
+ */
 
 const fetcher = async <ResponseType,>(url: string): Promise<ResponseType> => {
   const response = await fetch(url);
@@ -26,30 +30,34 @@ const fetcher = async <ResponseType,>(url: string): Promise<ResponseType> => {
 };
 
 const useActivity = (): ActivityResponse => {
-  const github = useSWR<GitHubResponse>('/api/github-events', fetcher);
+  // const github = useSWR<GitHubResponse>('/api/github-events', fetcher);
   const vercel = useSWR<VercelResponse>('/api/vercel', fetcher);
-  const twitter = useSWR<TwitterResponse>('/api/twitter', fetcher);
+  // const twitter = useSWR<TwitterResponse>('/api/twitter', fetcher);
   const [status, setStatus] = useState<ActivityResponse>({
     emoji: '🤔',
     status: 'Not sure',
   });
 
   useEffect(() => {
-    if (!twitter.error && twitter.data?.active) {
-      setStatus({
-        emoji: '📱',
-        status: `tweeting on Twitter`,
-      });
-      return;
-    }
+    /*
+     * if (!twitter.error && twitter.data?.active) {
+     *   setStatus({
+     *     emoji: '📱',
+     *     status: `tweeting on Twitter`,
+     *   });
+     *   return;
+     * }
+     */
 
-    if (!github.error && github.data?.active) {
-      setStatus({
-        emoji: '👨‍💻',
-        status: 'coding on Github',
-      });
-      return;
-    }
+    /*
+     * if (!github.error && github.data?.active) {
+     *   setStatus({
+     *     emoji: '👨‍💻',
+     *     status: 'coding on Github',
+     *   });
+     *   return;
+     * }
+     */
 
     if (!vercel.error && vercel.data?.active) {
       setStatus({
@@ -148,10 +156,12 @@ const useActivity = (): ActivityResponse => {
       status: 'working',
     });
   }, [
-    github.data?.active,
-    github.error,
-    twitter.data?.active,
-    twitter.error,
+    /*
+     * github.data?.active,
+     * github.error,
+     * twitter.data?.active,
+     * twitter.error,
+     */
     vercel.data?.active,
     vercel.error,
   ]);
